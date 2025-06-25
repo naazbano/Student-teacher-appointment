4c87f05f012887c3c9f0da50a2753af7c506240a
naazbano <naazb597@gmail.com>
2025-06-25 14:43:07 +0530

    project completed

---
+++ backend/models/Message.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    from:{
+        type:String,
+        required:true
+    },
+    to:{
+        type:String,
+        required:true
+    },
+    messageText:{
+        type:String,
+        required:true
+    }
+})
+
+const Message = mongoose.model('Message',messageSchema)
+
+module.exports = Message
+
+
+
